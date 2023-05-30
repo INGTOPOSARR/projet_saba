28c5d4d12fa15f474c9ed7959685f0c9d6ba370a
INGTOPOSARR <109141145+INGTOPOSARR@users.noreply.github.com>
2023-05-30 09:40:28 +0000

    Update sarr_web.js

--- resources/sarr_web.js
+++ resources/sarr_web.js
@@ -426,14 +426,15 @@ map.on("rendercomplete", function(evt) {
         var attributionList = attribution.getElementsByTagName('ul')[0];
         var firstLayerAttribution = attributionList.getElementsByTagName('li')[0];
         var qgis2webAttribution = document.createElement('li');
-        qgis2webAttribution.innerHTML = '<a href="https://github.com/tomchadwin/qgis2web">qgis2web</a> &middot; ';
+        qgis2webAttribution.innerHTML = '<a href="https://sarr-diouf-projetsabadola.netlify.app/">SARR</a> &middot; ';
         var olAttribution = document.createElement('li');
-        olAttribution.innerHTML = '<a href="https://openlayers.org/">OpenLayers</a> &middot; ';
+        olAttribution.innerHTML = '<a href="https://openlayers.org/">ABLAYE</a> &middot; ';
         var qgisAttribution = document.createElement('li');
-        qgisAttribution.innerHTML = '<a href="https://qgis.org/">QGIS</a>';
+        qgisAttribution.innerHTML = '<a href="https://qgis.org/">Contributor</a>';
         attributionList.insertBefore(qgis2webAttribution, firstLayerAttribution);
         attributionList.insertBefore(olAttribution, firstLayerAttribution);
         attributionList.insertBefore(qgisAttribution, firstLayerAttribution);
+        
         attributionComplete = true;
     }
 })
